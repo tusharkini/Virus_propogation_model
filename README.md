179e4f1cc0016e0e9f7609ba794f92a3506f51b0
tusharkini <kini1998pv@gmail.com>
2022-11-14 04:18:11 -0500

    Add part 3 code and results

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Virus_propogation_model
+# CSC591 Virus Propogation Model
+> This project implementation is done towards fullfillment of Project 5 of CSC 591 Graph Data Mining
+
+## Reaserch Paper
+The implementation is inspired by the paper given in `research_paper` folder. The paper is [Got the Flu? (or Mumps) Check the Eigenvalue!](https://arxiv.org/pdf/1004.0060v1.pdf)
+
+## Goal
+To implement the given prolem and objectives stated [here](https://github.com/tusharkini/Virus_propogation_model/blob/main/objective/P4VirusPropagation.pdf). Analyze the propagation of a virus in a network and prevent a networkwide epidemic. In order to do that, your team will need to:
+- Analyze and understand a virus propagation model.
+- Calculate the effective strength of a virus.
+- Simulate the propagation of a virus in a network.
+- Implement immunization policies to prevent a virus from spreading across a network. 
+
+
+---
+## Data  
+
+You will be provided with the following materials in advance:
+- Supplementary material on virus propagation.
+- Parameter values for experiments:
+    - Transmission probabilities β 1 = 0.20 and β 2 = 0.01.
+    - Healing probabilities δ 1 =0.70 and δ 2 = 0.60.
+    - Number of available vaccines k 1 = 200.
+- Static contact network (i.e., one undirected unweighted graph) for Option 1:
+    - static.network
+
+---
+
+
+## Getting Started
+
+### Installation
+
+- Install Python3 from [here](https://www.python.org/downloads/) and finish the required setup in the executable file.
+- Install pip package manager for future downloads-
+    ```bash
+    $ python -m ensurepip --upgrade
+    ```
+- Upgrade the version of pip-
+    ```
+    $ python -m pip install --upgrade pip
+    ```
+- Install NetworkX for graph processing-
+    ```bash
+    $ pip install networkx
+    ```
+- Upgrade the version of networkx-
+    ```
+    $ pip install --upgrade networkx
+    ```
+
+- Create working directory named `Virus_propogation_P5` and go inside it
+    ```bash
+    $ mkdir Virus_propogation_P5
+    $ cd Virus_propogation_P5
+    ```
+- Clone this repository from [here](https://github.com/tusharkini/Virus_propogation_model) or use the following in GitBash
+    ```bash
+    $ git clone https://github.com/tusharkini/Virus_propogation_model
+    ```
+### Running the Algorithm Code
+- Run the algorithm code using- 
+    ```bash
+    $ python main.py 
+    ```
+    This will create a series of images in the `results` folder. These plot names are self explainatory in nature.
+
+## Authors
+
+- Tushar Kini [Github](https://github.com/tusharkini)
+
+---
+
+## Tech Stack
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+
+-----
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from operator import itemgetter
+from copy import copy, deepcopy
 
 random.seed(100)
 
@@ -16,7 +17,6 @@ Cvpm2 = beta2 / delta2
 
 def compute_largest_eigen_value(adjacency_matrix):
     eigen_value, eigen_vector = np.linalg.eig(adjacency_matrix)
-    #eig_set = [(eigenvalue[i], eigenvector[i]) for i in range(len(eigenvalue))]
     eigen_value = sorted(eigen_value, reverse=True)
     #take only the real part of the complex eigen value
     return eigen_value[0].real
@@ -24,7 +24,7 @@ def compute_largest_eigen_value(adjacency_matrix):
 def static_delta(eigen_value, delta,title):
     print(f"Computing minimum transmission probability with delta = {delta}")
     # creating the test data
-    x = np.linspace(0, 1,1000)
+    x = np.linspace(0.001, 1,1000)
     y=(eigen_value*x)/delta
 
     # rendering the chart
@@ -43,7 +43,7 @@ def static_delta(eigen_value, delta,title):
 
 def static_beta(eigen_value, beta, title):
     # creating the test data
-    x = np.linspace(0, 1,1000)
+    x = np.linspace(0.001, 1,1000)
     y=(eigen_value*beta)/x
 
     # rendering the chart
@@ -55,15 +55,123 @@ def static_beta(eigen_value, beta, title):
     plt.savefig(f'./results/{title}.png', bbox_inches='tight')
     plt.close()
     # for there to be epidemic we would have s>1
-    #thus we get min beta with s==1 so beta will be greater than this value
+    #thus we get max delta with s==1 so delta will be lesser than this value
     max_delta = (eigen_value*beta)/1.0
     if max_delta >= 1.0 :
         return 1.0
     else:
         return max_delta
 
+
+def policy_A(adjacency_matrix, g, k):
+    immunized = set()
+    total_node = nx.number_of_nodes(g)
+    while len(immunized) < k:
+        # get a random node from the network
+        r = random.randint(0, total_node-1)
+        if r not in immunized:
+            immunized.add(r)
+    # vaccinate the chosed nodes, no edge so cannot infect anyone
+    for node in immunized:
+        for i in range(len(adjacency_matrix[node])):
+            adjacency_matrix[node][i] = 0
+            adjacency_matrix[i][node] = 0
+    return adjacency_matrix
+
+def policy_B(adjacency_matrix, g, k):
+    immunized = set()
+    # sort the nodes based on degree of node
+    degree = sorted(list(g.degree()), key=lambda x: x[1], reverse=1)
+    for i in range(k):
+        immunized.add(degree[i][0])
+
+    for node in immunized:
+        # vaccinate the chosen nodes, no edge so cannot infect anyone
+        for i in range(len(adjacency_matrix[node])):
+            adjacency_matrix[node][i] = 0
+            adjacency_matrix[i][node] = 0
+    return adjacency_matrix
+
+def policy_C(g, k):
+    immunized = set()
+    total_node = nx.number_of_nodes(g)
+    while len(immunized) < k:
+        # get the node with the highest degree
+        degree = sorted(list(g.degree()), key=lambda x: x[1], reverse=1)
+        immunized.add(degree[0][0])
+        #remove the node from the graph and iterate again
+        g.remove_node(degree[0][0])
+
+    # create empty adjacency matrix
+    new_adjacency_matrix = [[0 for _ in range(total_node)] for _ in range(total_node)]
+    # populate the adjacency matrix
+    for i in nx.edges(g):
+        new_adjacency_matrix[i[0]][i[1]] = 1
+        new_adjacency_matrix[i[1]][i[0]] = 1
+    return new_adjacency_matrix
+
+
+def policy_D(adjacency_matrix, g, k):
+    total_node = nx.number_of_nodes(g)
+    value, vector = np.linalg.eig(adjacency_matrix)
+    eigen_set = [(value[i], vector[i]) for i in range(len(value))]
+    eigen_set = sorted(eigen_set, key=lambda x: x[0], reverse=1)
+    #get the eigen vetcor for corresponding largest eigen value
+    largest_vector = eigen_set[0][1]
+    largest_vector = np.absolute(largest_vector)
+    # get the first k largest value in the eigen vector and get the corresponding node
+    target = [u[0] for u in sorted(enumerate(largest_vector), reverse=True, key=itemgetter(1))[:k]]
+    for i in target:
+        g.remove_node(i)
+    # create empty adjacency matrix
+    new_adjacency_matrix = []
+    for i in range(total_node):
+        row = []
+        for j in range(total_node):
+            row.append(0)
+        new_adjacency_matrix.append(row)
+    # populate the adjacency matrix
+    for i in nx.edges(g):
+        new_adjacency_matrix[i[0]][i[1]] = 1
+        new_adjacency_matrix[i[1]][i[0]] = 1
+    return new_adjacency_matrix
+
+def find_k_vaccine(adjacency_matrix, g, policy):
+    strengths = []
+    number_vaccines = []
+    step = 50
+    #when flag set to 0 we have found min k for which strength <=1
+    flag = 1
+    while flag:
+        number_vaccines.append(
+            step if not number_vaccines else number_vaccines[-1] + step)
+        k = number_vaccines[-1]
+        if policy == 'A':
+            new_adjacency = policy_A(adjacency_matrix.copy(), g.copy(), k)
+        elif policy == 'B':
+            new_adjacency = policy_B(adjacency_matrix.copy(), g.copy(), k)
+        elif policy == 'C':
+            new_adjacency = policy_C(g.copy(), k)
+        elif policy == 'D':
+            new_adjacency = policy_D(adjacency_matrix.copy(), g.copy(), k)
+        largest_eigen = compute_largest_eigen_value(new_adjacency)
+        strength = largest_eigen * Cvpm1
+        if strength < 1:
+            flag = 0
+        strengths.append(strength)
+    plt.plot(number_vaccines, strengths)
+    title = f"Strength vs varying k with beta={beta1} and delta={delta1} with Policy {policy}"
+    plt.title(title)
+    plt.xlabel('Number of vaccines k ')
+    plt.ylabel('Effective Strength s ')
+    plt.axhline(y=1, linestyle='--', linewidth=1, color='r')
+    plt.savefig(f'./results/{title}.png', bbox_inches='tight')
+    plt.close()
+    return number_vaccines[-1]
+
 def main():
     # Read the data from file and add edges to empty graph
+    print('OPTION 1 PART 1')
     g = nx.Graph()
     file = './datasets/static.network'
     with open(file) as f:
@@ -84,7 +192,7 @@ def main():
     for i in nx.edges(g):
         adjacency_matrix[i[0]][i[1]] = 1
         adjacency_matrix[i[1]][i[0]] = 1
-    
+    ''''
     largest_eigenvalue = compute_largest_eigen_value(adjacency_matrix)
     s1 = largest_eigenvalue * Cvpm1
     s2 = largest_eigenvalue * Cvpm2
@@ -111,7 +219,52 @@ def main():
         largest_eigenvalue, beta2, f'Strength vs varying delta with beta={beta2}')
     print(f"Maximum healing probability: {fixed_beta2_max_delta} with fixed beta={beta2}")
     pass
+    print('OPTION 1 PART 2')
 
+
+    print('OPTION 1 PART 3')
+    # Implements immunization policies
+    # Calculate the effective strength (s) of the virus on the immunized contact network
+    print("Immunization with Policy A")
+    adjacency_matrix_A = policy_A(adjacency_matrix.copy(), g.copy(), k=200)
+    largest_eigenvalue_A = compute_largest_eigen_value(adjacency_matrix_A)
+    strength_A = largest_eigenvalue_A * Cvpm1
+    print(f"Effective strengh of Policy A with beta = {beta1}, delta = {delta1} is: {strength_A}")
+
+    print("Immunization with Policy B")
+    adjacency_matrix_B = policy_B(adjacency_matrix.copy(), g.copy(), k=200)
+    largest_eigenvalue_B = compute_largest_eigen_value(adjacency_matrix_B)
+    strength_B = largest_eigenvalue_B * Cvpm1
+    print(f"Effective strengh of Policy B with beta = {beta1}, delta = {delta1} is: {strength_B}")
+'''
+    print("Immunization with Policy C")
+    adjacency_matrix_C = policy_C(g.copy(), k=200)
+    largest_eigenvalue_C = compute_largest_eigen_value(adjacency_matrix_C)
+    strength_C = largest_eigenvalue_C * Cvpm1
+    print(f"Effective strengh of Policy C with beta = {beta1}, delta = {delta1} is: {strength_C}")
+
+    print("Immunization with Policy D")
+    adjacency_matrix_D = policy_D(adjacency_matrix.copy(), g.copy(), k=200)
+    largest_eigenvalue_D = compute_largest_eigen_value(adjacency_matrix_D)
+    strength_D = largest_eigenvalue_D * Cvpm1
+    print(f"Effective strengh of Policy D with beta = {beta1}, delta = {delta1} is: {strength_D}")
+    
+    # Find minimum number of vaccines required k
+    print("Finding minimum number of vaccine k for policy A")
+    min_number_vaccines_A = find_k_vaccine(adjacency_matrix, g, 'A')
+    print(f'Minimum vaccines required for policy A with beta={beta1}, delta={delta1} is: {min_number_vaccines_A}')
+
+    print("Finding minimum number of vaccine k for policy B")
+    min_number_vaccines_B = find_k_vaccine(adjacency_matrix, g, 'B')
+    print(f'Minimum vaccines required for policy A with beta={beta1}, delta={delta1} is: {min_number_vaccines_B}')
+
+    print("Finding minimum number of vaccine k for policy C")
+    min_number_vaccines_C = find_k_vaccine(adjacency_matrix, g, 'C')
+    print(f'Minimum vaccines required for policy C with beta={beta1}, delta={delta1} is: {min_number_vaccines_C}')
+
+    print("Finding minimum number of vaccine k for policy D")
+    min_number_vaccines_D = find_k_vaccine(adjacency_matrix, g, 'D')
+    print(f'Minimum vaccines required for policy D with beta={beta1}, delta={delta1} is: {min_number_vaccines_D}')
 
 if __name__ == "__main__":
     main()
